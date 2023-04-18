c9941107bd05e9fa4673be3c2aa2486e132235f6
Emre Ergül <emredevsalot@gmail.com>
2023-04-18 21:55:19 +0300

    test

--- src/Composition.tsx
+++ src/Composition.tsx
@@ -5,7 +5,7 @@ import {Title} from './Title';
 
 export const MyComposition = () => {
 	return (
-		<AbsoluteFill className="bg-gray-100 items-center justify-center">
+		<AbsoluteFill className="bg-gray-300 items-center justify-center">
 			<div className="m-10" />
 			<Logo />
 			<div className="m-3" />
